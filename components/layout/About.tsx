61e71c51d5f33b549478a22ca90b0dda10d19113
Ronald Tanner <rtanner@msuite.tech>
2024-02-24 09:43:01 -0500

    start of the about

--- components/layout/About.tsx
+++ components/layout/About.tsx
@@ -1,40 +1,96 @@
 import { Statistics } from "./Statistics";
-import pilot from "../../assets/pilot.png";
 import Image from "next/image";
 
 export const About = () => {
-  return (
-    <section id="about" className="container py-24 sm:py-32">
-      <div className="bg-muted/50 border rounded-lg py-12">
-        <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
-          <Image
-            src={pilot}
-            width={300}
-            height={300}
-            alt=""
-            className="w-[300px] object-contain rounded-lg"
-          />
-          <div className="bg-green-0 flex flex-col justify-between">
-            <div className="pb-6">
-              <h2 className="text-3xl md:text-4xl font-bold">
-                <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-                  About{" "}
-                </span>
-                Company
-              </h2>
-              <p className="text-xl text-muted-foreground mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit.
-              </p>
-            </div>
+	return (
+		<section id="about" className="container py-24 sm:py-32">
+			<div className="bg-muted/50 border rounded-lg py-12">
+				<div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
+					<Image
+						src={"/graphic1.svg"}
+						width={300}
+						height={300}
+						alt=""
+						className="w-[300px] object-contain rounded-lg"
+					/>
+					<div className="bg-green-0 flex flex-col justify-between">
+						<div className="pb-6">
+							<h2 className="text-3xl md:text-4xl font-bold">
+								<span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+									About{" "}
+								</span>
+								Company
+							</h2>
+							<p>
+								<strong>Tanner Technology</strong> bridges the gap between
+								cutting-edge AI and impactful web development. We are a team of
+								passionate experts dedicated to crafting{" "}
+								<strong>intelligent solutions</strong> that empower businesses
+								to
+								<strong> thrive in the digital age.</strong>
+							</p>
+							<br></br>
+							<h3>
+								<strong>We Combine:</strong>
+							</h3>
+							<br></br>
+							<ul>
+								<li>
+									<strong>The power of AI:</strong>
+									<ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+										<li>
+											Machine learning for personalized experiences and{" "}
+											<strong>automated workflows</strong>
+										</li>
+										<li>
+											Data-driven insights to{" "}
+											<strong>optimize performance</strong> and{" "}
+											<strong>drive results.</strong>
+										</li>
+									</ul>
+								</li>
+								<br></br>
+								<li>
+									<strong>Exceptional modern web development:</strong>
+									<ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+										<li>
+											<strong>Intuitive and responsive</strong> user interfaces.
+										</li>
+										<li>
+											<strong>Scalable and secure</strong> solutions tailored to
+											your specific needs.
+										</li>
+									</ul>
+								</li>
+							</ul>
+							<br></br>
+							<h3>
+								<strong>Our Focus:</strong>
+							</h3>
+							<ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+								<li>
+									<strong>Innovation:</strong> We stay at the forefront of AI
+									and web development trends, continuously exploring new
+									possibilities to deliver{" "}
+									<strong>cutting-edge solutions</strong>.
+								</li>
+								<li>
+									<strong>Collaboration:</strong> We partner closely with our
+									clients to understand their unique challenges and
+									<strong> co-create</strong> solutions that drive success.
+								</li>
+								<li>
+									<strong>Results:</strong> We are driven by{" "}
+									<strong>measurable outcomes</strong> and strive to deliver{" "}
+									<strong>real-world impact</strong> for our clients.
+								</li>
+							</ul>
+						</div>
 
-            <Statistics />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+						<Statistics />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
--- components/layout/Statistics.tsx
+++ components/layout/Statistics.tsx
@@ -1,41 +1,38 @@
 export const Statistics = () => {
-  interface statsProps {
-    quantity: string;
-    description: string;
-  }
+	interface statsProps {
+		quantity: string;
+		description: string;
+	}
 
-  const stats: statsProps[] = [
-    {
-      quantity: "2.7K+",
-      description: "Users",
-    },
-    {
-      quantity: "1.8K+",
-      description: "Subscribers",
-    },
-    {
-      quantity: "112",
-      description: "Downloads",
-    },
-    {
-      quantity: "4",
-      description: "Products",
-    },
-  ];
+	const stats: statsProps[] = [
+		{
+			quantity: "32",
+			description: "Applications Made",
+		},
+		{
+			quantity: "25",
+			description: "Solutions Developed",
+		},
+		{
+			quantity: "16",
+			description: "Companies Helped",
+		},
+		{
+			quantity: "4",
+			description: "Products in the Works",
+		},
+	];
 
-  return (
-    <section id="statistics">
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
-        {stats.map(({ quantity, description }: statsProps) => (
-          <div
-            key={description}
-            className="space-y-2 text-center"
-          >
-            <h2 className="text-3xl sm:text-4xl font-bold ">{quantity}</h2>
-            <p className="text-xl text-muted-foreground">{description}</p>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section id="statistics">
+			<div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+				{stats.map(({ quantity, description }: statsProps) => (
+					<div key={description} className="space-y-2 text-center">
+						<h2 className="text-3xl sm:text-4xl font-bold ">{quantity}</h2>
+						<p className="text-xl text-muted-foreground">{description}</p>
+					</div>
+				))}
+			</div>
+		</section>
+	);
 };

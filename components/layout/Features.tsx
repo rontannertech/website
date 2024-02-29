4b84d040647d4241303dbc902cc2bf9a37ab8c58
Ronald Tanner <rtanner@msuite.tech>
2024-02-29 16:24:29 -0500

    2 more sections

--- components/layout/Features.tsx
+++ components/layout/Features.tsx
@@ -1,94 +1,94 @@
 import { Badge } from "../ui/badge";
 import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card";
-import image from "../../assets/growth.png";
-import image3 from "../../assets/reflecting.png";
-import image4 from "../../assets/looking-ahead.png";
+import image from "@/public/human-centered.png";
+import image2 from "@/public/security-compliance.png";
+import image3 from "@/public/scalability-futureproofing.png";
 import Image, { StaticImageData } from "next/image";
 
 interface FeatureProps {
-  title: string;
-  description: string;
-  image: StaticImageData;
+	title: string;
+	description: string;
+	image: StaticImageData;
 }
 
 const features: FeatureProps[] = [
-  {
-    title: "Responsive Design",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
-    image: image4,
-  },
-  {
-    title: "Intuitive user interface",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
-    image: image3,
-  },
-  {
-    title: "AI-Powered insights",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
-    image: image,
-  },
+	{
+		title: "Human Centered Approach",
+		description:
+			"Our AI solutions are designed to augment human capabilities rather than replace them. We focus on how our services can empower people to work more efficiently and creatively.",
+		image: image,
+	},
+	{
+		title: "Secure and Compliant",
+		description:
+			"We emphasize our commitment to data security and compliance with relevant regulations. Building trust with potential clients by demonstrating our robust security practices.",
+		image: image2,
+	},
+	{
+		title: "Scalable and Future Proofed",
+		description:
+			"Demonstrating our ability to scale our solutions to meet the growing needs of clients. Showing our commitment to staying ahead of the curve by implementing future-proof technologies that can adapt to new challenges.",
+		image: image3,
+	},
 ];
 
 const featureList: string[] = [
-  "Dark/Light theme",
-  "Reviews",
-  "Features",
-  "Pricing",
-  "Contact form",
-  "Our team",
-  "Responsive design",
-  "Newsletter",
-  "Minimalist",
+	"Dark/Light Themes",
+	"3D Web Features",
+	"Cutting-Edge Expertise",
+	"Transparency",
+	"Iterative Development",
+	"Thought Leadership",
+	"Responsive Design",
+	"Data-Driven Approach",
+	"Collaborative Partnerships",
 ];
 
 export const Features = () => {
-  return (
-    <section id="features" className="container py-24 sm:py-32 space-y-8">
-      <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
-        Many{" "}
-        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-          Great Features
-        </span>
-      </h2>
+	return (
+		<section id="features" className="container py-24 sm:py-32 space-y-8">
+			<h2 className="text-3xl lg:text-4xl font-bold md:text-center">
+				Many{" "}
+				<span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+					Great Features
+				</span>
+			</h2>
 
-      <div className="flex flex-wrap md:justify-center gap-4">
-        {featureList.map((feature: string) => (
-          <div key={feature}>
-            <Badge variant="secondary" className="text-sm">
-              {feature}
-            </Badge>
-          </div>
-        ))}
-      </div>
+			<div className="flex flex-wrap md:justify-center gap-4">
+				{featureList.map((feature: string) => (
+					<div key={feature}>
+						<Badge variant="secondary" className="text-sm">
+							{feature}
+						</Badge>
+					</div>
+				))}
+			</div>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {features.map(({ title, description, image }: FeatureProps) => (
-          <Card key={title}>
-            <CardHeader>
-              <CardTitle>{title}</CardTitle>
-            </CardHeader>
+			<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+				{features.map(({ title, description, image }: FeatureProps) => (
+					<Card key={title}>
+						<CardHeader>
+							<CardTitle>{title}</CardTitle>
+						</CardHeader>
 
-            <CardContent>{description}</CardContent>
+						<CardContent>{description}</CardContent>
 
-            <CardFooter>
-              <Image
-                src={image}
-                alt="About feature"
-                className="w-[200px] lg:w-[300px] mx-auto"
-              />
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
-    </section>
-  );
+						<CardFooter>
+							<Image
+								src={image}
+								alt="About feature"
+								className="w-[200px] lg:w-[300px] mx-auto"
+							/>
+						</CardFooter>
+					</Card>
+				))}
+			</div>
+		</section>
+	);
 };
